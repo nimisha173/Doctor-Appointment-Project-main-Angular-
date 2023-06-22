3b6efa078f2a6d1e4b96fa7458a7f66776621e5e
Nimisha Reghuvaran Sheeba(UST <245212@ust.com>
2023-06-22 17:35:53 +0530

    main

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdministratorComponent } from './administrator/administrator.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ReviewComponent } from './review/review.component';
+import { UserSignupComponent } from './user/user-signup/user-signup.component';
+import { UserloginComponent } from './user/userlogin/userlogin.component';
+import { UserloginmainComponent } from './userloginmain/userloginmain.component';
+
+const routes: Routes = [
+  {path:'',component:HomepageComponent},
+  {path:'admin',component:AdminloginComponent},
+  {path:'administrator',component:AdministratorComponent},
+  {path:'appointment',component:AppointmentComponent},
+  {path:'user-login',component:UserloginComponent},
+  {path:'userloginmain',component:UserloginmainComponent},
+  {path:'user-signup',component:UserSignupComponent},
+  {path:'review',component:ReviewComponent}
+ 
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
